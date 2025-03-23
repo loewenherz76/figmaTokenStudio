267746094a6b9955328b4bfa9b42d7512f7930ba
Leonhard Bolschakow <leonhard@bolschakow.de>
2025-03-23 22:00:51 +0100

    create typoscript

---
+++ ext_emconf.php
@@ -0,0 +1,28 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array (
+    'title' => 'Figma Token Studio',
+    'description' => 'Figma Token Studio - Test Template',
+    'category' => 'template',
+    'version' => '1.0.0',
+    'state' => 'beta',
+    'uploadfolder' => false,
+    'createDirs' => NULL,
+    'clearcacheonload' => true,
+    'author' => 'Leonhard Bolschakow',
+    'author_email' => 'leonhard@bolschakow.de',
+    'author_company' => '',
+    'constraints' =>
+        array (
+            'depends' =>
+                array (
+                    'typo3' => '7.4.0-13.99.99',
+                ),
+            'conflicts' =>
+                array (
+                ),
+            'suggests' =>
+                array (
+                ),
+        ),
+);
